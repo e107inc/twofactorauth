ba832c6bb92c439f47230e0058941913ffa135fc
Moc <github@tijnkuyper.nl>
2021-01-16 16:58:01 +0100

    Code clean-up

--- login.php
+++ login.php
@@ -7,18 +7,24 @@
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
  */
+require_once("../../class2.php");
 
 // Make this page inaccessible when plugin is not installed. 
-/*if (!e107::isInstalled('twofactorauth'))
+if (!e107::isInstalled('twofactorauth'))
 {
 	e107::redirect();
 	exit;
-}*/
+}
 
-require_once("../../class2.php");
+$session_user_id = e107::getSession('2fa')->get('user_id');
 
-// No need to access this file directly. 
-// TODO check if redirected from core login, else redirect to core login first. 
+// No need to access this file directly or when already logged in. 
+if(empty($session_user_id) || USER) // TODO - check if this check suffices
+{
+	e107::getRender()->tablerender("Access denied", "No direct access permitted! Please use the login form."); // redirect to /login.php?
+	require_once(FOOTERF);
+	exit;
+}
 
 e107_require_once(e_PLUGIN.'twofactorauth/vendor/autoload.php');
 use \RobThree\Auth\TwoFactorAuth;
@@ -26,18 +32,13 @@ $tfa_library = new TwoFactorAuth();
 
 require_once(HEADERF);
 
-$sql = e107::getDb();
-$tp  = e107::getParser();
-$frm = e107::getForm(); 
-
 $text = "";
-
 
 // Process TOTP code and verify against secret key
 if(isset($_POST['enter-totp-process']))
 {
 	// Retrieve user ID from session 
-	$user_id 	= e107::getSession('2fa')->get('user_id');
+	$user_id = e107::getSession('2fa')->get('user_id');
 	error_log("Session User ID: ".$user_id);
 
 	// Retrieve secret_key of this user, stored in the database
@@ -45,7 +46,7 @@ if(isset($_POST['enter-totp-process']))
 	error_log("Secret key: ".$secret_key);
 
 	// Set $totp, entered by user
-	$totp = $_POST['totp'];
+	$totp = $_POST['totp']; // TODO check input for digits only
 	error_log("TOTP entered: ".$totp);
 
 	// Check if the entered TOTP is correct. 
@@ -73,6 +74,9 @@ if(isset($_POST['enter-totp-process']))
 		// Get previous page the user was on before logging in. 
 		$redirect_to = e107::getSession('2fa')->get('previous_page');
 		error_log("Session Previous page: ".$redirect_to); 
+
+		// Clear session data
+		e107::getSession('2fa')->clearData();
 
 		// Redirect to previous page or otherwise to homepage
 		if($redirect_to)
@@ -105,11 +109,10 @@ $form_options = array(
 $text .= e107::getForm()->open('enter-totp');
 $text .= e107::getForm()->text("totp", "", 80, $form_options);
 $text .= e107::getForm()->button('enter-totp-process', "Submit 2FA code");
-$text .= e107::getFOrm()->close(); 
+$text .= e107::getForm()->close(); 
 
 // Let's render and show it all!
 e107::getRender()->tablerender("Two Factor Authenthication", e107::getMessage()->render().$text);
-
 
 require_once(FOOTERF);
 exit;
--- test.php
+++ test.php
@@ -29,25 +29,24 @@ $tp  = e107::getParser();
 
 $tfa = new TwoFactorAuth("test");
 
+$text = '';
 
        
 
-echo '<li>First create a secret and associate it with a user';
+$text .= '<li>First create a secret and associate it with a user';
 $secret = $tfa->createSecret(160);  // Though the default is an 80 bits secret (for backwards compatibility reasons) we recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)
-echo '<li>Next create a QR code and let the user scan it:<br><img src="' . $tfa->getQRCodeImageAsDataUri('My label', $secret) . '"><br>...or display the secret to the user for manual entry: ' . chunk_split($secret, 4, ' ');
+$text .= '<li>Next create a QR code and let the user scan it:<br><img src="' . $tfa->getQRCodeImageAsDataUri('My label', $secret) . '"><br>...or display the secret to the user for manual entry: ' . chunk_split($secret, 4, ' ');
 $code = $tfa->getCode($secret);
-echo '<li>Next, have the user verify the code; at this time the code displayed by a 2FA-app would be: <span style="color:#00c">' . $code . '</span> (but that changes periodically)';
-echo '<li>When the code checks out, 2FA can be / is enabled; store (encrypted?) secret with user and have the user verify a code each time a new session is started.';
-echo '<li>When aforementioned code (' . $code . ') was entered, the result would be: ' . (($tfa->verifyCode($secret, $code) === true) ? '<span style="color:#0c0">OK</span>' : '<span style="color:#c00">FAIL</span>');
+$text .= '<li>Next, have the user verify the code; at this time the code displayed by a 2FA-app would be: <span style="color:#00c">' . $code . '</span> (but that changes periodically)';
+$text .= '<li>When the code checks out, 2FA can be / is enabled; store (encrypted?) secret with user and have the user verify a code each time a new session is started.';
+$text .= '<li>When aforementioned code (' . $code . ') was entered, the result would be: ' . (($tfa->verifyCode($secret, $code) === true) ? '<span style="color:#0c0">OK</span>' : '<span style="color:#c00">FAIL</span>');
 
 
 
     
-/*
-$text = "testing";
+
 // Let's render and show it all!
 e107::getRender()->tablerender("2FA", $text);
-*/
 
 require_once(FOOTERF);
 exit;
